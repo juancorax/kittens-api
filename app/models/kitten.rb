033638a5944012f3f2a4897bd32b63c447180808
Juan Manuel Corigliano Giuliani <126042007+juancorax@users.noreply.github.com>
2024-11-16 16:26:12 -0300

    feat: create views for KittensController actions
    Add validations to Kitten model.
    Implement a display of the flash hash.

--- app/models/kitten.rb
+++ app/models/kitten.rb
@@ -1,2 +1,6 @@
 class Kitten < ApplicationRecord
+  validates :name, length: { in: 2..20 }, presence: true
+  validates :age, inclusion: { in: 1..20, message: "must be between 1 and 20" }, presence: true
+  validates :cuteness, inclusion: { in: 1..10, message: "must be between 1 and 10" }, presence: true
+  validates :softness, inclusion: { in: 1..10, message: "must be between 1 and 10" }, presence: true
 end
